ef059ea7f8ee8c503789722d67c2e2378561c6ef
Frederic Depuydt <frederic.depuydt@outlook.com>
2023-08-17 21:59:13 +0200

    Auto-Commit by Rebuild script

--- __init__.py
+++ __init__.py
@@ -88,7 +88,7 @@ class OpenHABSkill(OVOSSkill):
         self.speak_dialog("hello.world")
 
 
-    @intent_handler(IntentBuilder("OnOffCommandIntent").require("OnOffCommandKeyword").require("OnOffCommandRegex"))
+    @intent_handler(IntentBuilder("OnOffCommandIntent").require("OnOffCommandKeyword").require("OnOffCommandRegex").require("Command").require("Item"))
     def handle_on_off_command_intent(self, message):
         self.speak_dialog('Hooray')
         pprint(vars(message))
